2f7074016eabc4b6cbf30597dd6025f604cf6892
Judith van Stegeren <git@jd7h.com>
2016-09-20 16:19:54 +0200

    Logging for the scraper!

--- src/data_set.py
+++ src/data_set.py
@@ -1,50 +1,51 @@
 import glob
 import math
 import json
+import logging
 
 def load(path):
-    # open the earlier results                                                   
-     data = []                                                                    
-     data_set_paths = glob.glob(path)                      
-     # open them in order                                                         
-     data_set_paths.sort(key=lambda path : int(path.split("_")[1].split(".")[0])) 
-     print(data_set_paths)
-     for path in data_set_paths:                                                  
-         print("Opening",path)                                                    
-         try:                                                                     
-             with open(path,"r") as infile:                                       
-                 data_set = json.load(infile)                                     
-         except Exception as e:                                                   
-             print(type(e))                                                       
-             print(e)
-             print("Error in dataset",path)                                       
-             continue                                                             
-         else:                                                                    
-             data.extend(data_set)                                                
-     print("Dataset contains",len(data),"records.")             
-     return data
+    logging.info('Opening %s',path)
+    data = []
+    data_set_paths = glob.glob(path)
+    # open them in order
+    data_set_paths.sort(key=lambda path : int(path.split("_")[1].split(".")[0]))
+    logging.debug('Found files in %s: %s',path, str(data_set_paths))
+    for p in data_set_paths:
+        logging.info('Opening %s',p)
+        try:
+            with open(p,"r") as infile:
+                data_set = json.load(infile)
+        except Exception as e:
+            logging.error("%s",repr(e))
+            logging.error("Error with dataset %s",p)
+            continue
+        else:
+            data.extend(data_set)
+            logging.info("Dataset contains %d records",len(data))
+    return data
 
-def dump(data,pathprefix,data_set_size=1000):                                                                  
-     # dump data to seperate files to fix memory errors                                                                     
-     nr_of_sets = math.floor(len(data)/data_set_size)                             
-     print("Will dump to",nr_of_sets,"datasets.")                                 
-                                                                                  
-     # dump all datasets                                                          
-     for i in range(0,nr_of_sets):                                                
-         start = i * data_set_size                                                
-         end = (1+i) * data_set_size                                              
-         data_sub_set = data[start:end]                                           
-         data_sub_set_nr = str(i)                                                 
-         data_sub_set_name = pathprefix + data_sub_set_nr + ".json"    
-         print("Dumping data[" + str(start) + ":" + str(end) + "] to " + data_sub_set_name)
-         with open(data_sub_set_name,"w+") as outfile:                            
-             json.dump(data_sub_set,outfile)                                      
-                                                                                  
-     # last dataset with leftovers                                                
-     start = nr_of_sets * data_set_size                                           
-     end = len(data)                                                              
-     data_sub_set_name = pathprefix + str(nr_of_sets) + ".json"        
-     data_sub_set = data[start:end]                                               
-     print("Dumping data[" + str(start) + ":" + str(end) + "] to " + data_sub_set_name)
-     with open(data_sub_set_name,"w+") as outfile:                                
-         json.dump(data_sub_set,outfile)
+def dump(data,pathprefix,data_set_size=1000):
+    logging.info("Dumping data of length %d",len(data))
+    logging.info("Target path is %s with max size %d",pathprefix,data_set_size)
+    nr_of_sets = math.floor(len(data)/data_set_size)
+
+    # dump all datasets
+    for i in range(0,nr_of_sets):
+        start = i * data_set_size
+        end = (1+i) * data_set_size
+        data_sub_set = data[start:end]
+        data_sub_set_nr = str(i)
+        data_sub_set_name = pathprefix + data_sub_set_nr + ".json"
+        logging.info("Dumping data[%d:%d] to %s",start,end,data_sub_set_name)
+        with open(data_sub_set_name,"w+") as outfile:
+           json.dump(data_sub_set,outfile)
+
+    # last dataset with leftovers
+    start = nr_of_sets * data_set_size
+    end = len(data)
+    data_sub_set_name = pathprefix + str(nr_of_sets) + ".json"
+    data_sub_set = data[start:end]
+    logging.info("Dumping data[%d:%d] to %s",start,end,data_sub_set_name)
+    with open(data_sub_set_name,"w+") as outfile:
+        json.dump(data_sub_set,outfile)
+    logging.info("Dumped data to %d files",nr_of_sets)
--- src/scraper_refs.py
+++ src/scraper_refs.py
@@ -10,6 +10,8 @@ import filtered_filetypes
 import glob
 import math
 import data_set
+import logging
+import datetime
 
 # betere aanpak
 # verzamel alle links die moeten worden gescrapet: url en referencing advisories
@@ -26,13 +28,15 @@ def open_url(u):
     for ext in exclude:
         if ext in u["url"]:
             u["status"] = ext
-            print(u["url"],"contains",ext,"\tskipping...")
+            logging.warning("%s contains %s",u["url"],ext)
             return u
 
     if not ("http://" in u["url"] or "https://" in u["url"]):
+        logging.warning("%s does not contains http:// or https://",u["url"])
+        logging.warning("adding http:// to %s",u["url"])
         u["url"] = "http://" + u["url"]
 
-    print("Scraping", u["url"], "...")
+    logging.info("Scraping %s ...", u["url"])
     try:
         conn = urllib.request.urlopen(
             urllib.request.Request(u["url"], headers={
@@ -40,23 +44,26 @@ def open_url(u):
                 advisories, http://github.com/jd7h/pritchard)'}))
     except urllib.error.HTTPError as error:
         # Return code error (e.g. 404, 501, ...)
+        logging.debug("HTTP-code %s",error.code)
         u["status"] = error.code
     except Exception as e:
-        print("Error for", u["url"],repr(e))
+        logging.error("Error for %s: %s", u["url"],repr(e))
         u["status"] = repr(e)
     else:
         u["status"] = conn.getcode()
+        logging.debug("HTTP-code %s",conn.getcode())
         try:
+            logging.debug("Reading contents of %s",u["url"])
             u["content"] = conn.read()
         except Exception as e:
-            print("Error for", u["url"],repr(e))
+            logging.error("Read error for %s: %s", u["url"],repr(e))
             u["status"] = str(u["status"]) + ", " + repr(e)
     finally:
         return u
 
 def scrape_references(references):
     """Take a set of references and scrape the urls of not-yet-scraped references"""
-    print("Scraping references...")
+    logging.info("Scraping references...")
     already_visited = 0
     scraped = 0
     backup_interval = 100
@@ -65,10 +72,10 @@ def scrape_references(references):
     for idx,ref in enumerate(references):
         # write intermediary results to backup file
         if scraped % backup_interval == 0 and scraped != 0:
-            print("Dumping",scraped,"new results to backup.")
+            logging.info("Dumping %d new results to backup.",scraped)
             data_set.dump(references,"../data/temp/temp_",data_set_size)
         if idx % save_interval == 0 and idx != 0:
-            print("Dumping",idx,"processed results to data set.")
+            logging.info("Dumping %d processed results to data set.",idx)
             data_set.dump(references,"../data/references_",data_set_size)
         # scrape unvisited urls
         if ref["status"] == 0:
@@ -78,9 +85,9 @@ def scrape_references(references):
         else:
             already_visited += 1
     #print stats
-    print("Done scraping.")
-    print("Scraped:", scraped)
-    print("Already visited:", already_visited)
+    logging.info("Done scraping.")
+    logging.info("Scraped: %d", scraped)
+    logging.info("Already visited: %d", already_visited)
     data_set.dump(references,"../data/references_",data_set_size)
     return references
 
@@ -93,31 +100,33 @@ def build_url_set(advisory_path,data_set_path):
     if os.path.isfile(advisory_path):
         with open(advisory_path, "r") as infile:
             advisories = json.load(infile)
-            print("Loaded", len(advisories), "previously scraped advisories.")
+            logging.info("Loaded %d previously scraped advisories", len(advisories))
 
     # open earlier scraped references and urls
     old_references = []
     old_urls = []
 
     old_references = data_set.load(data_set_path)
-    print("Loaded", len(old_references), "previously scraped references.")
+    logging.info("Loaded %d previously scraped references", len(old_references))
     old_urls = [u["url"] for u in old_references]
 
     #make set of new urls
     new_urls = [url for url in set(url_extractor.get_all_urls(advisories)) if url not in old_urls]
     new_references = [{'url':u, 'status':0, 'content':''} for u in new_urls]
 
-    print("old urls", len(old_urls))
-    print("new urls", len(new_urls))
+    logging.info("%d old urls from datasets in %s",len(old_urls),data_set_path)
+    logging.info("%d new urls from advisories in %s",len(new_urls),advisory_path)
 
     all_references = old_references + new_references
     
     return all_references
 
 def main():
-    print("Building data set.")
+    logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s',filename='scraper_refs.log',level=logging.DEBUG)
+    logging.info("------------ SCRAPER REFS -----------------")
+    logging.info("Building data set.")
     data = build_url_set("primary_advisories.json","../data/references_*.json")
-    print("Start scraping phase.")
+    logging.info("Start scraping phase.")
     scrape_references(data)
     return data
 
