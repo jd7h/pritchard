6e91405d415a93140b44ab0c27f37e370fa95a33
Judith van Stegeren <git@jd7h.com>
2016-09-20 13:40:34 +0200

    module for dealing with loading and saving data sets

---
+++ src/data_set.py
@@ -0,0 +1,43 @@
+def load(path):
+    # open the earlier results                                                   
+     data = []                                                                    
+     data_set_paths = glob.glob(path)                      
+     # open them in order                                                         
+     data_set_paths.sort(key=lambda path : int(path.split("_")[1].split(".")[0])) 
+     for path in data_set_paths:                                                  
+         print("Opening",path)                                                    
+         try:                                                                     
+             with open(path,"r") as infile:                                       
+                 data_set = json.load(infile)                                     
+         except Exception as e:                                                   
+             print(type(e))                                                       
+             print("Error in dataset",path)                                       
+             continue                                                             
+         else:                                                                    
+             data.extend(data_set)                                                
+     print("Dataset contains",len(data),"records.")             
+
+def dump(data,pathprefix,data_set_size=1000):                                                                  
+     # dump data to seperate files to fix memory errors                                                                     
+     nr_of_sets = math.floor(len(data)/data_set_size)                             
+     print("Will dump to",nr_of_sets,"datasets.")                                 
+                                                                                  
+     # dump all datasets                                                          
+     for i in range(0,nr_of_sets):                                                
+         start = i * data_set_size                                                
+         end = (1+i) * data_set_size                                              
+         data_sub_set = data[start:end]                                           
+         data_sub_set_nr = str(i)                                                 
+         data_sub_set_name = pathprefix + data_sub_set_nr + ".json"    
+         print("Dumping data[" + str(start) + ":" + str(end) + "] to " + data_sub_set_name)
+         with open(data_sub_set_name,"w+") as outfile:                            
+             json.dump(data_sub_set,outfile)                                      
+                                                                                  
+     # last dataset with leftovers                                                
+     start = nr_of_sets * data_set_size                                           
+     end = len(data)                                                              
+     data_sub_set_name = pathprefix + str(nr_of_sets) + ".json"        
+     data_sub_set = data[start:end]                                               
+     print("Dumping data[" + str(start) + ":" + str(end) + "] to " + data_sub_set_name)
+     with open(data_sub_set_name,"w+") as outfile:                                
+         json.dump(data_sub_set,outfile)
