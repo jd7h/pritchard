ce8073d751b4536767c13c5da03a1c43908bd2ed
Judith van Stegeren <git@jd7h.com>
2016-09-20 12:20:22 +0200

    fixed too large json files

--- src/brokenjsonfix.py
+++ src/brokenjsonfix.py
@@ -12,8 +12,7 @@ def fix():
 
     return data
 
-def main():
-    data = fix()
+def score(data):
     slecht = 0
     goed = 0
     complete = []
@@ -25,16 +24,40 @@ def main():
         else:
             goed += 1
             complete.append(d)
+    print("current score")
+    print("goed:",goed)
+    print("slecht:",slecht)
+    return goed,slecht,complete,incomplete
 
+def dump(data):
     # dump data to seperate files to fix memory errors
     data_set_size = 1000
-    for i in range(0,floor(len(data)/data_set_size)):
+    nr_of_sets = math.floor(len(data)/data_set_size)
+    print("Will dump to",nr_of_sets,"datasets.")
+    
+    # dump all datasets
+    for i in range(0,nr_of_sets):
         start = i * data_set_size
-        end = (1+i) * data_size_size
-        data_sub_set = data[start:finish]
+        end = (1+i) * data_set_size
+        data_sub_set = data[start:end]
         data_sub_set_nr = str(i)
-        data_sub_set_name = "references_" + data_sub_set_nr
-        print("Dumping data[" + str(start) + ":" + str(finish) + "] to " + data_sub_set_name)
+        data_sub_set_name = "../data/references_" + data_sub_set_nr + ".json"
+        print("Dumping data[" + str(start) + ":" + str(end) + "] to " + data_sub_set_name)
         with open(data_sub_set_name,"w+") as outfile:
             json.dump(data_sub_set,outfile)
+    
+    # last dataset with leftovers
+    start = nr_of_sets * data_set_size
+    end = len(data)
+    data_sub_set_name = "../data/references_" + str(nr_of_sets) + ".json"
+    data_sub_set = data[start:end]
+    print("Dumping data[" + str(start) + ":" + str(end) + "] to " + data_sub_set_name)
+    with open(data_sub_set_name,"w+") as outfile:
+        json.dump(data_sub_set,outfile)
 
+'''
+def main():
+    data = fix()
+    score(data)
+    dump(data)
+'''
